65f0fa6f9db4a5e09c712d334d6e0ca0d81fa426
Sanchosdisaster <sanchosdisaster@mail.ru>
2020-06-29 20:11:16 +0300

    Получил кнопки и инпуты

---
+++ js-app/src/js/js.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let startBtn = document.getElementById('start');
+let bugetValue = document.getElementsByClassName('budget-value');
+let dayBugetValue = document.getElementsByClassName('daybudget-value');
+let levelValue = document.getElementsByClassName('level-value');
+let expensesValue = document.getElementsByClassName('expenses-value');
+let optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value');
+let incomeValue = document.getElementsByClassName('income-value');
+let monthsavingsValue = document.getElementsByClassName('monthsavings-value');
+let yearSavingsValue = document.getElementsByClassName('yearsavings-value');
+let expensesItem = document.getElementsByClassName('expenses-item');
+let btn = document.getElementsByTagName('button');
+let expensesItembtn = btn[1];
+let optionalExpensesBtn = btn[2];
+let countBugetBtn = btn[3];
+let optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item');
+
+
+
+
+
+
---
+++ js-app/src/js/main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let startBtn = document.getElementById('start');
+let bugetValue = document.getElementsByClassName('budget-value');
+let dayBugetValue = document.getElementsByClassName('daybudget-value');
+let levelValue = document.getElementsByClassName('level-value');
+let expensesValue = document.getElementsByClassName('expenses-value');
+let optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value');
+let incomeValue = document.getElementsByClassName('income-value');
+let monthsavingsValue = document.getElementsByClassName('monthsavings-value');
+let yearSavingsValue = document.getElementsByClassName('yearsavings-value');
+let expensesItem = document.getElementsByClassName('expenses-item');
+let btn = document.getElementsByTagName('button');
+let expensesItemBtn = btn[0];
+let optionalExpensesBtn = btn[1];
+let countBugetBtn = btn[2];
+let optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item');
+let chooseIncome = document.querySelector('.choose-income');
+let checkSavings = document.querySelector('.checksavings');
+let sum = document.querySelector('.choose-sum');
+let percent = document.querySelector('.choose-percent');
+let year = document.querySelector('.year-value');
+let month = document.querySelector('.month-value');
+let day = document.querySelector('.day-value');
+
+
+
+
+
+
+
+
